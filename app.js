b60f456c26aeeeef3ff4a41d36b589549953ad62
Baudouin Flament <baudouinflament@gmail.com>
2021-09-21 18:19:29 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+const morgan = require('morgan');
+const fs = require('fs');
+
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+mongoose.connect('mongodb+srv://baudouin:14071099Bf@cluster0.igpyz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use(bodyParser.json());
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStream }));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
+
+module.exports = app;
---
+++ middleware/validate-input.js
@@ -0,0 +1,61 @@
+const joi = require('@hapi/joi');
+
+const userSchema = joi.object({
+    email: joi.string().trim().email().required(),
+    password: joi.string().trim().min(4).required()
+});
+exports.user = (req, res, next) => {
+    const {error, value} = userSchema.validate(req.body);
+    if (error) {
+        res.status(422).json({ error: "email ou mot de passe invalide" });
+    } else {
+        next();
+    }
+};
+
+const sauceSchema = joi.object({
+    userId: joi.string().trim().length(24).required(),
+    name: joi.string().trim().min(1).required(),
+    manufacturer: joi.string().trim().min(1).required(),
+    description: joi.string().trim().min(1).required(),
+    mainPepper: joi.string().trim().min(1).required(),
+    heat: joi.number().integer().min(1).max(10).required()
+})
+exports.sauce = (req, res, next) => {
+    let sauce;
+    if (req.file) {
+        sauce = JSON.parse(req.body.sauce);
+    } else {
+        sauce = req.body;
+    }
+    
+    const {error, value} = sauceSchema.validate(sauce);
+    if (error) {
+        res.status(422).json({ error: "Les données entrées sont invalides" });
+    } else {
+        next();
+    }
+}
+
+const idSchema = joi.string().trim().length(24).required();
+exports.id = (req, res, next) => {
+    const {error, value} = idSchema.validate(req.params.id);
+    if (error) {
+        res.status(422).json({ error: "id de la sauce invalide" });
+    } else {
+        next();
+    }  
+}
+
+const likeSchema = joi.object({
+    userId: joi.string().trim().length(24).required(),
+    like: joi.valid(-1, 0, 1).required()
+});
+exports.like = (req, res, next) => {
+    const {error, value} = likeSchema.validate(req.body);
+    if (error) {
+        res.status(422).json({ error: "Les données entrées sont invalides" });
+    } else {
+        next();
+    }
+};
